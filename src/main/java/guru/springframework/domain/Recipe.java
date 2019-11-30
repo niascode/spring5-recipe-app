08d49cd6bdb1f2c5c887d8129445b43c1f20b60d
JFrum <niasadmin@niasnebraska.com>
2019-11-30 16:57:39 -0600

    Updated with Category Join

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -29,7 +29,11 @@ public class Recipe {
     private Difficulty difficulty;
     @OneToMany(cascade = CascadeType.ALL ,mappedBy = "recipe")
     private Set<Ingredient> ingredients;
-
+    @ManyToMany
+    @JoinTable(name="recipe_category"
+            ,joinColumns = @JoinColumn(name="recipe_id")
+            ,inverseJoinColumns = @JoinColumn(name="category_id"))
+    private Set<Category> categories;
 
     @Lob
     private Byte[] image;
@@ -121,5 +125,13 @@ public class Recipe {
 
     public void setDifficulty(Difficulty difficulty) {
         this.difficulty = difficulty;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
