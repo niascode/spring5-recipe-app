ab2f12e16e5c4860ae45ca788465ea870eb0f80b
JFrum <niasadmin@niasnebraska.com>
2019-11-30 15:45:43 -0600

    Adding Notes and Recipe models.

--- src/main/java/guru/springframework/Spring5RecipeAppApplication.java
+++ src/main/java/guru/springframework/Spring5RecipeAppApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Spring5RecipeAppApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(Spring5RecipeAppApplication.class, args);
+		System.out.println("Hello World!");
 	}
 }
