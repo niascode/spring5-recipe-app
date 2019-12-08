c99c67e91b6e657440fbe66489382ee90ddea460
JFrum <niasadmin@niasnebraska.com>
2019-12-08 11:20:37 -0600

    Added an integration test

--- src/test/java/guru/springframework/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/controllers/IndexControllerTest.java
@@ -7,15 +7,20 @@ import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.junit.Assert.*;
-import static org.mockito.ArgumentMatchers.anySet;
-import static org.mockito.ArgumentMatchers.eq;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
 
 public class IndexControllerTest {
     @Mock
@@ -30,9 +35,8 @@ public class IndexControllerTest {
         MockitoAnnotations.initMocks(this);
         controller = new IndexController(recipeService);
     }
-    @Test
-    public void getIndexPage() {
 
+    private Set<Recipe> getRecipeReturnObject(){
         Set<Recipe> recipes = new HashSet<>();
         Recipe r1 = new Recipe();
         r1.setId(1L);
@@ -40,12 +44,23 @@ public class IndexControllerTest {
         Recipe r2 = new Recipe();
         r1.setId(2L);
         recipes.add(r2);
+        return recipes;
+    }
 
+    @Test
+    public void testMockMVC() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
 
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
+    }
+
+    @Test
+    public void getIndexPage() {
+        Set<Recipe> recipes = getRecipeReturnObject();
         when(recipeService.getRecipes()).thenReturn(recipes);
-
         ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
-
         String retValue = controller.getIndexPage(model);
         assertEquals("index",retValue);
         assertEquals(2,recipes.size());
