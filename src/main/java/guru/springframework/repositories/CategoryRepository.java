afadbf72e11a564a962104d25001513fe1e69879
JFrum <niasadmin@niasnebraska.com>
2019-12-08 10:03:25 -0600

    Updated Index Controller tests.

--- src/main/java/guru/springframework/repositories/CategoryRepository.java
+++ src/main/java/guru/springframework/repositories/CategoryRepository.java
@@ -1,9 +1,6 @@
 package guru.springframework.repositories;
 
 import guru.springframework.domain.Category;
-import org.hibernate.annotations.SQLInsert;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
--- src/main/java/guru/springframework/repositories/UnitOfMeasureRepository.java
+++ src/main/java/guru/springframework/repositories/UnitOfMeasureRepository.java
@@ -2,7 +2,6 @@ package guru.springframework.repositories;
 
 import guru.springframework.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
-
 import java.util.Optional;
 
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure,Long> {
