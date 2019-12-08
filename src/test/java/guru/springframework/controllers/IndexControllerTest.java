c1cad4a522137d8b3fe86d7cad0b314417393c16
JFrum <niasadmin@niasnebraska.com>
2019-12-08 10:20:49 -0600

    Added more unit tests.

--- src/test/java/guru/springframework/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/controllers/IndexControllerTest.java
@@ -1,17 +1,21 @@
 package guru.springframework.controllers;
 
+import guru.springframework.domain.Recipe;
 import guru.springframework.services.RecipeService;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.Assert.*;
 import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 public class IndexControllerTest {
     @Mock
@@ -28,9 +32,24 @@ public class IndexControllerTest {
     }
     @Test
     public void getIndexPage() {
-    String retValue = controller.getIndexPage(model);
-    assertEquals("index",retValue);
-    verify(recipeService,times(1)).getRecipes();
-    verify(model,times(1)).addAttribute(eq("recipes"),anySet());
+
+        Set<Recipe> recipes = new HashSet<>();
+        Recipe r1 = new Recipe();
+        r1.setId(1L);
+        recipes.add(r1);
+        Recipe r2 = new Recipe();
+        r1.setId(2L);
+        recipes.add(r2);
+
+
+        when(recipeService.getRecipes()).thenReturn(recipes);
+
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+        String retValue = controller.getIndexPage(model);
+        assertEquals("index",retValue);
+        assertEquals(2,recipes.size());
+        verify(recipeService,times(1)).getRecipes();
+        verify(model,times(1)).addAttribute(eq("recipes"),argumentCaptor.capture());
     }
 }
