37a50091e8d5595135d80f84216170e851751069
Frum, Jereomy K <Jereomy.Frum@TDAmeritrade.com>
2019-12-09 06:32:52 -0600

    Added some category tests to test Lombok plugin at work.  Also testing Circle.ci

--- src/test/java/guru/springframework/domain/CategoryTest.java
+++ src/test/java/guru/springframework/domain/CategoryTest.java
@@ -3,11 +3,14 @@ package guru.springframework.domain;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.Assert.*;
 
 public class CategoryTest {
 
-    Category category;
+    public Category category;
 
     @Before
     public void setUp(){
@@ -23,9 +26,18 @@ public class CategoryTest {
 
     @Test
     public void getDescription() {
+        String myDesc = "What my description is...";
+        category.setDescription(myDesc);
+        assertEquals(myDesc,category.getDescription());
     }
 
     @Test
     public void getRecipes() {
+        Set<Recipe> recipeSet = new HashSet<>();
+        Recipe rec1 = new Recipe();
+        rec1.setId(1L);
+        recipeSet.add(rec1);
+        category.setRecipes(recipeSet);
+        assertEquals(recipeSet.size(),1);
     }
 }
