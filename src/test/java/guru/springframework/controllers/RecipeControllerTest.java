6e3d36d26a199972b000c35a873046acdc18a6b1
JFrum <niasadmin@niasnebraska.com>
2019-12-28 09:20:07 -0600

    Customized 404 error page 234

--- src/test/java/guru/springframework/controllers/RecipeControllerTest.java
+++ src/test/java/guru/springframework/controllers/RecipeControllerTest.java
@@ -109,13 +109,12 @@ public class RecipeControllerTest {
 
         verify(recipeService,times(1)).deleteById(anyLong());
     }
+
     @Test
     public void testGetRecipeNotFound() throws Exception {
-
         when(recipeService.findById(anyLong())).thenThrow(NotFoundException.class);
-
         mockMvc.perform(get("/recipe/1/show"))
-                .andExpect(status().isNotFound());
+                .andExpect(status().isNotFound())
+                .andExpect(view().name("404error"));
     }
-
 }
